2c6d1d0f8dce44dd1bf91583e3f6fac5a6277e21
asaliu <asaliu08@gmail.com>
2019-07-20 11:28:39 +0800

    Add files via upload

---
+++ mysite5/user/urls.py
@@ -0,0 +1,12 @@
+
+# file : user/urls.py
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^login$', views.login_view),
+    url(r'^logout$', views.logout_view),
+    url(r'^reg$', views.reg_view),
+]
+
